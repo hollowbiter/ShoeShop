c85d86e1a230de6e7eb3c8154cbaed3c51298ce6
Valeriy Gilderman <sekretbot@gmail.com>
2026-03-11 19:14:05 +0300

    Fix 0.2

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ COLOR_OUT_OF_STOCK = "lightblue"
 
 
 
-
 # Класс базы данных
 class Database:
     def __init__(self, db_name):
@@ -367,6 +366,7 @@ class LoginWindow(tk.Frame):
             row=0, column=0, sticky="w", padx=5, pady=5
         )
 
+        
         vcmd = (self.register(lambda new_value: len(new_value) <= 50), "%P")
         self.entry_login = EntryWithContextMenu(
             frame, font=("Times New Roman", 12), validate="key", validatecommand=vcmd
@@ -755,11 +755,9 @@ class OrdersWindow(tk.Frame):
         self.app = app
         self.pack(fill=tk.BOTH, expand=True)
 
-        
         top_frame = tk.Frame(self, bg=COLOR_EXTRA_BG, height=40)
         top_frame.pack(fill=tk.X, side=tk.TOP)
         top_frame.pack_propagate(False)
-
 
         user_text = "Гость"
         if self.app.current_user:
@@ -814,13 +812,15 @@ class OrdersWindow(tk.Frame):
             for row in rows:
                 self.create_order_card(row)
         except Exception as e:
+
             tk.Label(self.scrollable_frame, text=f"Ошибка базы данных: {e}", fg="red", bg=COLOR_MAIN_BG).pack()
 
     def create_order_card(self, row):
-       
+        
         card = tk.Frame(self.scrollable_frame, bg=COLOR_MAIN_BG, relief=tk.RIDGE, bd=2)
         card.pack(fill=tk.X, padx=20, pady=10, ipady=10)
 
+        
         left_box = tk.Frame(card, bg=COLOR_MAIN_BG)
         left_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10)
 
@@ -831,6 +831,7 @@ class OrdersWindow(tk.Frame):
         tk.Label(left_box, text=f"Адрес пункта выдачи: {addr}", font=("Times New Roman", 11), bg=COLOR_MAIN_BG, anchor="w", wraplength=600).pack(fill=tk.X)
         tk.Label(left_box, text=f"Дата заказа: {row[1]}", font=("Times New Roman", 11), bg=COLOR_MAIN_BG, anchor="w").pack(fill=tk.X)
 
+        
         right_box = tk.Frame(card, bg=COLOR_MAIN_BG, relief=tk.SOLID, bd=1, width=150)
         right_box.pack(side=tk.RIGHT, fill=tk.Y, padx=10, pady=5)
         right_box.pack_propagate(False)
@@ -838,10 +839,12 @@ class OrdersWindow(tk.Frame):
         tk.Label(right_box, text="Дата доставки", font=("Times New Roman", 10), bg=COLOR_MAIN_BG).pack(pady=(5, 0))
         tk.Label(right_box, text=f"{row[2]}", font=("Times New Roman", 11, "bold"), bg=COLOR_MAIN_BG).pack(expand=True)
 
+        
         if self.app.current_user and self.app.current_user["role"] == "Администратор":
             btn_frame = tk.Frame(left_box, bg=COLOR_MAIN_BG)
             btn_frame.pack(anchor="w", pady=(10, 0))
             
+
             order_id = row[0]
             tk.Button(btn_frame, text="Редактировать", command=lambda i=order_id: OrderEditWindow(self.app, self, i), bg=COLOR_ACCENT).pack(side=tk.LEFT, padx=2)
             tk.Button(btn_frame, text="Удалить", command=lambda i=order_id: self.delete_order(i), bg=COLOR_ACCENT).pack(side=tk.LEFT, padx=2)
